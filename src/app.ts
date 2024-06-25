558f68e394f19b781b98d6a773fb12ee51a09855
Animesh <2025.cse18.animeshbag@gmail.com>
2024-06-25 16:37:13 +0530

    Project initialized integrating typescript express

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+})
+
+export default app;
